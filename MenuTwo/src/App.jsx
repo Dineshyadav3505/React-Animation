af738a1932a56984ca7af79bbdd98a73e719b3e6
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-07-29 23:17:13 +0530

    Refactor code for improved performance

---
+++ MenuTwo/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './Compontents/NavBar'
+
+const App = () => {
+  return (
+    <div className='h-screen bg-[#2e2a2a]' >
+      <NavBar/>
+    </div>
+  )
+}
+
+export default App
