72f27038f2a60c598dde84a5e6fe9fbc3d28217e
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-07-29 23:23:54 +0530

    Add menu functionality to NavBar component

--- MenuTwo/src/Compontents/NavBar.jsx
+++ MenuTwo/src/Compontents/NavBar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const NavBar = () => {
+
+    const [menu, setMenu] = useState(false)
   return (
     <>
-    <div className=""></div>
+    <div className="h-36 bg-transparent ">
+        <div className="h-[80%] bg-red-900"></div>
+        <div 
+            className="h-[20%] bg-slate-100"
+            onMouseEnter={() => setMenu(true)}
+            onMouseLeave={() => setMenu(false)}
+        >
+        </div>
+    </div>
     </>
   )
 }
